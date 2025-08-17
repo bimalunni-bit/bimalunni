ce9ed772b2c984d1be061f6210b30bf276f6f821
bimalunni-bit <bimalunni@gmail.com>
2025-08-17 19:48:49 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/kys', destination: '/kys/index.html' },
+      { source: '/kys/', destination: '/kys/index.html' },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
