6262b210d15e15497e2975bd6ad345f2dbf384ee
bimalunni <bimal@brkthroughmedia.com>
2025-08-17 16:09:21 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-white text-black">
+      <h1 className="text-5xl font-bold">Bimal Unni</h1>
+      <p className="mt-4 text-lg text-gray-600">
+        Welcome to my site. This is a work in progress 🚧
+      </p>
+    </main>
+  );
+}
